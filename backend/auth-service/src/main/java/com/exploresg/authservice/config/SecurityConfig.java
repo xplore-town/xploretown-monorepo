9381e9aa6a9dfb934c3aff322142bc0b81d03549
sree <sreeraj_ec@hotmail.com>
2025-11-18 08:02:29 +0800

    20251118-Auth-Service-H2-DB-Setup

--- backend/auth-service/src/main/java/com/exploresg/authservice/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/exploresg/authservice/config/SecurityConfig.java
@@ -12,7 +12,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf(csrf->csrf.disable())
+        http
+                .csrf(csrf->csrf.disable())
+                .headers(headers->headers.disable())
                 .authorizeHttpRequests(auth->auth
                         .requestMatchers(
                                 "/api/v1/check/**",
--- backend/auth-service/src/main/java/com/exploresg/authservice/controller/UserController.java
+++ backend/auth-service/src/main/java/com/exploresg/authservice/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.exploresg.authservice.controller;
 
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
