6e974c91b0c4262432e9292a9a0af0a85da29d06
sree <sreeraj_ec@hotmail.com>
2025-11-20 08:55:09 +0800

    20251120-frontend-turborepo-setup-initial-setup-completed

--- frontend/apps/user-app/src/App.tsx
+++ frontend/apps/user-app/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@exploresg.frontend/ui";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4">User App</h1>
+      <Button label="Click me" onClick={() => alert("Button clicked!")} />
+    </div>
+  );
 };
 
 export default App;
