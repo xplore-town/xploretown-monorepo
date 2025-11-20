d24b4c24114358f30a50f348e1c06e5f52cc1774
sree <sreeraj_ec@hotmail.com>
2025-11-20 18:08:02 +0800

    20251120-frontend-updated-navbar

--- frontend/apps/user-app/src/App.tsx
+++ frontend/apps/user-app/src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Footer, Navbar } from "@exploresg.frontend/ui";
 
+const userLinks = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+];
 const App = () => {
   return (
     <div className="flex min-h-screen flex-col">
-      <Navbar />
+      <Navbar logo="logo" links={userLinks} />
       <main className="flex-1">
         <Outlet />
       </main>
--- frontend/packages/ui/src/components/Navbar.tsx
+++ frontend/packages/ui/src/components/Navbar.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+export interface NavLink {
+  label: string;
+  href: string;
+}
+
+export interface NavbarProps {
+  logo: React.ReactNode;
+  links: NavLink[];
+}
+
+const Logo: React.FC<{ logo: React.ReactNode }> = ({ logo }) => {
+  return <div>{logo}</div>;
+};
+
+const Links: React.FC<{ links: NavLink[] }> = ({ links }) => {
+  return (
+    <div>
+      {links.map((link) => (
+        <Link to={link.href}>{link.label}</Link>
+      ))}
+    </div>
+  );
+};
+
+const Navbar: React.FC<NavbarProps> = ({ logo, links }) => {
+  return (
+    <nav className="border-b border-gray-200 bg-white shadow-md">
+      <div className="mx-auto max-w-7xl bg-red-300">
+        <Logo logo={logo} />
+        <Links links={links} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
